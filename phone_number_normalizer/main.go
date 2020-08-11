caa23350a25a742e42516ee1781cbbd247697f9d
Josh Nelson <joshcdnelson@gmail.com>
2020-08-11 07:40:29 -0400

    Gophercise 8: Phone Number Normalizer

---
+++ phone_number_normalizer/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	phonedb "gophercises/phone_number_normalizer/db"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "joshnelson"
+	password = "Kingdirk41"
+	dbname   = "gophercises_phone"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	must(phonedb.Reset("postgres", psqlInfo, dbname))
+
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	must(phonedb.Migrate("postgres", psqlInfo))
+
+	db, err := phonedb.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+
+	err = db.Seed()
+	must(err)
+
+	phones, err := db.AllPhones()
+	must(err)
+	for _, p := range phones {
+		fmt.Printf("Working on...%+v\n", p)
+		number := normalize(p.Number)
+		if number != p.Number {
+			fmt.Println("Updating or removing...", number)
+			existing, err := db.FindPhone(number)
+			must(err)
+			if existing != nil {
+				must(db.DeletePhone(p.ID))
+			} else {
+				p.Number = number
+				must(db.UpdatePhone(&p))
+			}
+		} else {
+			fmt.Println("No changes required")
+		}
+	}
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func normalize(phone string) string {
+	var buf bytes.Buffer
+
+	for _, ch := range phone {
+		if ch >= '0' && ch <= '9' {
+			buf.WriteRune(ch)
+		}
+	}
+
+	return buf.String()
+}
